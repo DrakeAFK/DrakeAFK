cfeb3ce0b4a2030f28408366e889c7a3630b0a86
DrakeAFK <drakehopkins@outlook.com>
2023-10-18 00:47:51 -0400

    v2 repo merge

---
+++ src/data.ts
@@ -0,0 +1,57 @@
+export const navData = {
+    nav: [
+        {
+          name: 'DrakeAFK',
+          path: '/'
+        },
+        {
+          name: 'Blog',
+          path: 'https://blog.drakeafk.io'
+        },
+        {
+          name: 'Projects',
+          path: '/projects'
+        },
+        {
+          name: 'About',
+          path: '/about'
+        },
+        {
+          name: 'Contact',
+          path: '/contact'
+
+        }
+    ]
+};
+
+export const links = {
+  github: {
+    name: 'Github',
+    path: 'https://github.com/drakeafk',
+  },
+  linkedin: {
+    name: 'Linkedin',
+    path: 'https://www.linkedin.com/in/drakehopkins/',
+  },
+  twitter: {
+    name: 'Twitter',
+    path: 'https://twitter.com/drakeafk_',
+  },
+  instagram: {
+    name: 'Instagram',
+    path: 'https://www.instagram.com/drakeafk_',
+  },
+  youtube: {
+    name: 'Youtube',
+    path: 'https://www.youtube.com/@drakeafk',
+  },
+  bioSocialLinks: {
+    name: 'BioSocialLinks',
+    path: 'https://links.drakeafk.io',
+  },
+  email: {
+    name: 'Email',
+    path: 'mailto:drake@drakeafk.io',
+  }
+};
+
